75f19c731c86a6dbc448997e422c8b9b39233d99
Jonator3 <joey.pehlke@stud.uni-due.de>
2023-07-18 13:54:25 +0200

    add corpus based measures

--- 201.Scripts/analyze.py
+++ 201.Scripts/analyze.py
@@ -5,31 +5,28 @@ import numpy as np
 from datetime import datetime
 
 import text_similarity
+import corpusVariance
 import parallelprozessing
 
 
+def getMeanLen(df):
+    len_list = [len(str(val)) for val in df["Value"]]
+    meanLen = sum(len_list) / len(len_list)
+    return meanLen
 
-#TODO: ungünstige Benennung, weil Value hier ja auch schon die Bezeichnung für die einzelnen Antworten ist.
-#TODO: Etas mehr Doku: wozu ist die step-size da?
-def getMeanVal(df, func):
-    step_size = 24
+
+def getMeanMeasureValue(df, func):
     values = [str(val) for val in df["Value"]]
 
-    def run(start):
-        total = 0
-        t_len = 0
-        for si in range(min(step_size, len(values)-start)):
-            v0 = values[start+si]
-            for vi in values[start+si+1:]:
-                total += func(v0, vi)[1]
-            t_len += len(values) - 1 - (start + si)
-        return total, t_len
+    total = 0
+    t_len = 0
+    for si in range(len(values)):
+        v0 = values[si]
+        for vi in values[si+1:]:
+            total += func(v0, vi)[1]
+        t_len += len(values) - 1 - si
 
-    tasks = [[i] for i in range(0, len(values)-1, step_size)]
-    res = parallelprozessing.run_mono(run, tasks)
-    total_val = sum([r[0] for r in res])
-    total_count = sum([r[1] for r in res])
-    return total_val / total_count  # mean val
+    return total / t_len  # mean val
 
 
 df = pd.read_csv("../051.Data/411.merged_CSV/data.CSV", sep=";")
@@ -43,31 +40,43 @@ table = pd.pivot_table(df, values='Value', index=['Language'], columns=['Variabl
 print(table)
 print("\n__________________________")
 
-exit()
+#exit()
 
 #TODO: alle Maße auch noch auf String-Ebene
-measures = {
+text_based_measures = {
     "GST": text_similarity.gst,
     "LCS": text_similarity.longest_common_substring,
     "Levenstein": text_similarity.levenshtein_distance,
     "VCos": text_similarity.vector_cosine
 }
 
-df_var = pd.DataFrame(columns = ["Language", "Variable", "numAnswers", "meanLen", "FractionUnique"] + ["mean"+m for m in measures.keys()])
+corpus_based_measures = {
+    "meanLen": getMeanLen,
+    "FractionUnique": corpusVariance.getFractionUnique,
+    "TTR": corpusVariance.computeTTR
+}
+
+
+def run_task(lang, var):
+    df_prompt = df[(df['Language'] == lang) & (df['Variable'] == var)]
+    print(lang, var, len(df_prompt))
+
+    cb_vals = [corpus_based_measures.get(m)(df_prompt) for m in corpus_based_measures.keys()]
+    tb_vals = [getMeanMeasureValue(df_prompt, text_based_measures.get(m)) for m in text_based_measures.keys()]
+
+    return [lang, var, len(df_prompt)] + cb_vals + tb_vals
+
+
+tasks_args = []
+
+df_var = pd.DataFrame(columns = ["Language", "Variable", "numAnswers"] + [m for m in corpus_based_measures.keys()] + ["mean" + m for m in text_based_measures.keys()])
 for lang in df['Language'].unique():
     #print(lang)
     for var in df['Variable'].unique():
-        #print(var)
-        df_prompt = df[(df['Language'] == lang) & (df['Variable'] == var)]
-        #if len(df_prompt) > 3000:
-        #    continue
-        print(lang, var, len(df_prompt))
+        tasks_args.append((lang, var))
 
-        len_list = [len(str(val)) for val in df_prompt["Value"]]
-        meanLen = sum(len_list)/len(len_list)
-        fractionUnique = getFractionUnique(df_prompt)
-
-        measure_vals = [getMeanVal(df_prompt, measures.get(m)) for m in measures.keys()]
-        #print("{:.4f}".format(num))
-        df_var.loc[len(df_var.index)] = [lang, var, len(df_prompt), meanLen, fractionUnique] + measure_vals
+results = parallelprozessing.run_mono(run_task, tasks_args)
+#print("{:.4f}".format(num))
+for res in results:
+    df_var.loc[len(df_var.index)] = res
 df_var.to_csv('variance.tsv', sep="\t")
--- 201.Scripts/corpusVariance.py
+++ 201.Scripts/corpusVariance.py
@@ -4,7 +4,12 @@ import random
 from statistics import mean
 from datetime import datetime
 
-#This file contains variance metrics considering not pairwise similarity (between texts in a corpus) but metrics wherethe whole corpus is needed as input.
+
+# This file contains variance metrics considering not pairwise similarity (between texts in a corpus) but metrics wherethe whole corpus is needed as input.
+
+tokensPerSample = 1000
+iterations = 1000
+
 
 # a very basic variance measure: the percentage of unique answers.
 # there are indeed completely repetitive answers, this is not an artifact of large numbers of empty answers
@@ -15,11 +20,13 @@ def getFractionUnique(df):
     # print(all, unique)
     return unique/all
 
+
 def computeIndividualTTR(tokenList):
     return len(set(tokenList))/len(tokenList)
 
+
 # sample a specific number of tokens multiple times and compute TTR for each subsample, return the average
-def computeTTR(df, tokensperSample, iterations):
+def computeTTR(df, tokenspersample=tokensPerSample, iter=iterations):
     values = [str(val) for val in df["Value"]]
     all_tokens = []
     for value in values:
@@ -28,8 +35,8 @@ def computeTTR(df, tokensperSample, iterations):
         all_tokens = all_tokens + tokens
     # print(all_tokens)
     ttrs = []
-    for i in range(iterations):
-        sample = random.sample(all_tokens, tokensPerSample)
+    for i in range(iter):
+        sample = random.sample(all_tokens, tokenspersample)
         #print(sample)
         ttr = computeIndividualTTR(sample)
         ttrs.append(ttr)
@@ -38,23 +45,21 @@ def computeTTR(df, tokensperSample, iterations):
     return mean(ttrs)
 
 
-#Testcase
-print(datetime.now())
+if __name__ == "__main__":
+    #Testcase
+    print(datetime.now())
 
-tokensPerSample = 1000
-iterations = 1000
-
-df = pd.read_csv("../051.Data/411.merged_CSV/data.CSV", sep=";")
-df_var = pd.DataFrame(columns = ["Language", "Variable", "numAnswers", "FractionUnique", "TTR"])
-for lang in df['Language'].unique():
-    print(lang)
-    for var in df['Variable'].unique():
-        print(var)
-        df_prompt = df[(df['Language'] == lang) & (df['Variable'] == var)]
-        fractionUnique = getFractionUnique(df_prompt)
-        ttr = computeTTR(df_prompt, tokensPerSample, iterations)
-        df_var.loc[len(df_var.index)] = [lang, var, len(df_prompt), fractionUnique, ttr]
-        # break # quit after the first language-prompt-pair
-    break  # quit after the first language
-df_var.to_csv('variance_corpus.tsv', sep="\t")
-print(datetime.now())
+    df = pd.read_csv("../051.Data/411.merged_CSV/data.CSV", sep=";")
+    df_var = pd.DataFrame(columns = ["Language", "Variable", "numAnswers", "FractionUnique", "TTR"])
+    for lang in df['Language'].unique():
+        print(lang)
+        for var in df['Variable'].unique():
+            print(var)
+            df_prompt = df[(df['Language'] == lang) & (df['Variable'] == var)]
+            fractionUnique = getFractionUnique(df_prompt)
+            ttr = computeTTR(df_prompt, tokensPerSample, iterations)
+            df_var.loc[len(df_var.index)] = [lang, var, len(df_prompt), fractionUnique, ttr]
+            # break # quit after the first language-prompt-pair
+        break  # quit after the first language
+    df_var.to_csv('variance_corpus.tsv', sep="\t")
+    print(datetime.now())
